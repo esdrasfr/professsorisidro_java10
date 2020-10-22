b24378a98d5b6682c42fc5195165696c415a9f00
professorisidro <isidro@professorisidro.com.br>
2020-10-22 17:50:44 -0300

    Exercicio Time

---
+++ ProjetoTime/src/Time.java
@@ -0,0 +1,50 @@
+
+public class Time {
+	private int hora;
+	private int minu;
+	private int segu;
+	
+	public String exibirHoraUniversal() {
+		return hora+":"+minu+":"+segu;
+	}
+	
+	public String exibirHoraPadrao() {
+		String strHora;
+		if (hora == 0) {
+			strHora = 12 + ":" + minu + ":" + segu +" AM";
+		}
+		else if (hora == 12) {
+			strHora = 12 + ":" + minu + ":" + segu + " PM";
+		}
+		else if (hora >= 1 && hora <= 11) {
+			strHora = hora + ":" + minu + ":" + segu +" AM";
+		}
+		else {
+			strHora = (hora-12) + ":" + minu + ":" + segu + " PM";
+		}
+		return strHora;
+	}
+	
+	
+	public int getHora() {
+		return hora;
+	}
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+	public int getMinu() {
+		return minu;
+	}
+	public void setMinu(int minu) {
+		this.minu = minu;
+	}
+	public int getSegu() {
+		return segu;
+	}
+	public void setSegu(int segu) {
+		this.segu = segu;
+	}
+	
+	
+
+}
---
+++ ProjetoTime/src/TimeTeste.java
@@ -0,0 +1,14 @@
+
+public class TimeTeste {
+	public static void main(String[] args) {
+		Time t = new Time();
+		
+		t.setHora(22);
+		t.setMinu(20);
+		t.setSegu(35);
+		
+		System.out.println(t.exibirHoraUniversal());
+		System.out.println(t.exibirHoraPadrao());
+	}
+
+}
