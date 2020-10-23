c7c962c8cb41859df0c305c608b4740db0380aac
professorisidro <isidro@professorisidro.com.br>
2020-10-23 17:28:17 -0300

    mais exemplos

--- ComparaStrings/src/Comparador.java
+++ ComparaStrings/src/Comparador.java
@@ -9,23 +9,23 @@ public class Comparador {
 		// quando eu uso o operador NEW, eu "forço" a Maquina Virtual a criar um novo bloco de memória
 		
 		String s1 = new String("Oi tudo bem? Quer batata?");
-		//String s2 = new String("Oi tudo bem? Quer batata?");
+		String s2 = new String("Oi tudo bem? Quer batata?");
 		
-		if (s1 == "Oi tudo bem? Quer batata?") {
+		if (s1 == s2) {
 			System.out.println("apontam para o mesmo objeto");
 		}
 		else {
 			System.out.println("apontam para objetos diferentes");
 		}
 		
-		/*
+		
 		if (s1.equals(s2)) {
 			System.out.println("Mesmo conteúdo");
 		}
 		else {
 			System.out.println("Conteúdos distintos");
 		}
-		*/
+		
 		
 
 	}
